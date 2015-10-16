04d7b603cc4f91eb4197388cfea91bc2dc2b56be
Cole Willsea <coleww@gmail.com>
2015-10-16 07:28:09 -0700

    no RT or @ business here

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ module.exports = function (config, name, func) {
     T.get('statuses/user_timeline', {count: 1, screen_name: name, exclude_replies: false}, function (err, data, response) {
       if (err) throw err
       console.log('grabbing timeline since:', data[0].id_str)
-      T.get('statuses/home_timeline', {count: 200, since_id: data[0].id_str}, function (err, data, response) {
+      T.get('statuses/home_timeline', {count: 200, since_id: data[0].id_str, exclude_replies: true}, function (err, data, response) {
         if (err) throw err
         console.log('got', data.length, 'tweets')
         data.some(function(tweet){
           console.log(tweet.text)
-          var res = tipots(tweet.text) && func(tweet)
+          var res = tipots(tweet.text) && !tweet.retweeted_status && func(tweet)
           if (res) {
             console.log('bingo', res)
             T.post('statuses/update', {status: res, in_reply_to_status_id: tweet.id_str}, function (err, data, response) {
